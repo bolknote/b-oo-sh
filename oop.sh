db812d11a277b8ca46a308256a96592d36964ee1
Евгений Степанищев <Evgeny.Stepanischev@tatar.ru>
2013-01-25 16:10:06 +0400

    Initial

---
+++ oop.sh
@@ -0,0 +1,63 @@
+. bash-@
+
+@Class Base
+    @Dim cnt
+
+    @Dim
+    __construct() {
+        This[cnt]=0
+    }
+
+    @Dim
+    IncCnt() {
+        let 'This[cnt]++'
+    }
+
+    @Dim
+    GetCnt() {
+        [ "$This[cnt]" -gt 1 ] && s=s
+        echo $Self said $This[cnt] time$s
+    }
+@End
+
+@Class Dog Base
+    @Dim
+    say() {
+        $This.IncCnt
+        echo 'Bow-wow!'
+    }
+@End
+
+@Class Car Base
+    @Dim
+    say() {
+        $This.IncCnt
+        echo 'Beep'
+    }
+@End
+
+@Class Proxy
+    @Dim obj
+
+    @Dim Static
+    getInstance() {
+        $1.New Self[obj]
+        @Ret $2 $Self[obj]
+    }
+@End
+
+
+Proxy.getInstance Car car
+Proxy.getInstance Dog dog
+
+$car.say
+$car.say
+$car.GetCnt
+
+$dog.say
+$dog.GetCnt
+
+
+
+
+
